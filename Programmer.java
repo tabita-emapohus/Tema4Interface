c5273c28f2531f65da208e15ef5e5b4d496361bf
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-05-17 23:46:00 +0300

    Create Programmer.java

---
+++ Programmer.java
@@ -0,0 +1,37 @@
+
+public class Programmer {
+
+	private String name;
+    private int age;
+
+    public Programmer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+ {
+
+	private String name;
+    private int age;
+
+    public Programmer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
